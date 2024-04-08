90db40766ad79197e0d7db129086388543c9e0bd
Jonathan Shamis <127160756+Johnnymaster100@users.noreply.github.com>
2024-04-08 21:25:03 +0000

    Solved Challenge 4

--- 01_restaurant/challenges/01_04_newcustomer_ch.sql
+++ 01_restaurant/challenges/01_04_newcustomer_ch.sql
@@ -4,3 +4,8 @@
 -- Anna Smith (asmith@samoca.org)
 -- 479 Lapis Dr., Memphis, TN
 -- Phone: (555) 555-1212; Birthday: July 21, 1973
+
+INSERT INTO Customers (FirstName, LastName, email, address, city, state, Phone, birthday)
+VALUES ('Ana', 'Smith', 'asmith@samoca.org', '479 Lapis Dr.', 'Memphis', 'TN', '555-555-1212', '1973-7-21');
+
+SELECT * FROM Customers ORDER BY CustomerID DESC;
