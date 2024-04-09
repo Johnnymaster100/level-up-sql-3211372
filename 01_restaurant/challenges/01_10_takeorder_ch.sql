67dd12af0873ab01441e64f2386db763cc2e2f62
Jonathan Shamis <127160756+Johnnymaster100@users.noreply.github.com>
2024-04-09 18:27:02 +0000

    Completed Challenge 10

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,39 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+SELECT * FROM Customers WHERE Address = '6939 Elka Place';
+
+INSERT INTO CustomersDishes (CustomerID, DishID)
+VALUES (70,
+  (SELECT DishID FROM Dishes WHERE Name = 'House Salad')
+  );
+
+INSERT INTO CustomersDishes (CustomerID, DishID)
+VALUES (70,
+  (SELECT DishID FROM Dishes WHERE Name = 'Mini Cheeseburgers')
+  );
+
+INSERT INTO CustomersDishes (CustomerID, DishID)
+VALUES (70,
+  (SELECT DishID FROM Dishes WHERE Name = 'Tropical Blue Smoothie')
+  );
+
+INSERT INTO Orders (CustomerID, OrderDate)
+VALUES (70, '2022-09-20 14:00:00');
+
+SELECT * FROM Orders WHERE CustomerID = 70;
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES (1001, 7);
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES (1001, 4);
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES (1001, 20);
+
+SELECT SUM(Dishes.Price) AS 'Total Price'
+FROM Dishes
+JOIN OrdersDishes ON Dishes.DishID = OrdersDishes.DishID
+WHERE OrdersDishes.OrderID = 1001;
