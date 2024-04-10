cf4dbd87638cab5cc1c9bb1942222d030057e7bf
Jonathan Shamis <127160756+Johnnymaster100@users.noreply.github.com>
2024-04-10 13:43:35 +0000

    Solved Challenge 6

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,9 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT COUNT(Loans.BookID) AS Count, Patrons.FirstName, Patrons.Email
+FROM Loans
+JOIN Patrons ON Patrons.PatronID = Loans.PatronID
+GROUP BY Loans.PatronID
+ORDER BY Count
+LIMIT 15;
