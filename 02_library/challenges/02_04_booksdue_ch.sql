7f2513f06bb49238c71e7524dd60102a0884065b
Jonathan Shamis <127160756+Johnnymaster100@users.noreply.github.com>
2024-04-10 13:30:22 +0000

    Solved Challenge 4

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,15 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT *
+FROM Loans
+WHERE ReturnedDate IS NULL
+AND DueDate = '2022-07-13';
+
+SELECT Loans.DueDate, Books.Title, Patrons.FirstName, Patrons.Email
+FROM Loans
+JOIN Books ON Books.BookID = Loans.BookID
+JOIN Patrons ON Patrons.PatronID = Loans.PatronID
+WHERE DueDate = '2022-07-13'
+AND Loans.ReturnedDate IS NULL;
